ea532ab7294688075cf7a852b11d04ac3a0a2a34
Jose Carlos Ramos <jramoscarmenates@gmail.com>
2021-07-20 14:55:24 -0400

    Adding new function "Sum".

---
+++ sum.js
@@ -0,0 +1,4 @@
+module.exports = (num1, num2) => {
+    // TODO: Add some number validations
+    return num1 + num2;
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
 const uppercase = require("./../uppercase")
+const sum = require("./../sum")
 
 test("uppercase hello retuns HELLO", () => {
     expect(uppercase("hello")).toBe("HELLO");
+});
+
+test("sum 1 + 5 returns 6", () => {
+    expect(sum(1, 5)).toBe(6);
 });
